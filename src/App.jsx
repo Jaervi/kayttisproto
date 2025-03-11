07551b79da5039afaf5c7e3db9c2314a831403b8
Oliver Niemi <oliver.niemi@aalto.fi>
2025-03-11 19:36:19 +0200

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import './App.css'
+import "./App.css";
 
 import React from "react";
 import "./App.css";
 import SettingsScreen from "./temp/components/SettingsScreen/SettingsScreen";
-import Home from "./temp/components/Home.jsx"
+import Home from "./temp/components/Home.jsx";
 import AlarmSetPage from "./temp/components/AlarmSetPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Home/>
-      <SettingsScreen />
-      <AlarmSetPage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/settings" element={<SettingsScreen />} />
+          <Route path="/alarmset" element={<AlarmSetPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
